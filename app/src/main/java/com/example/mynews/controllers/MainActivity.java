437cc856a3a28b5b3238dfbcef5a8845676b9578
Lumbo1379 <36245864+Lumbo1379@users.noreply.github.com>
2020-08-28 18:44:41 +0100

    Added toolbar

--- app/src/main/java/com/example/mynews/controllers/MainActivity.java
+++ app/src/main/java/com/example/mynews/controllers/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.mynews.controllers;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.mynews.R;
 import com.example.mynews.adapters.NewsViewPagerAdapter;
@@ -26,7 +30,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getViewComponents();
+        attachToolbar();
         initialiseViewPager();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_more:
+                return true;
+            case R.id.menu_activity_main_search:
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void getViewComponents() {
@@ -45,6 +70,11 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(mNewsViewPagerAdapter);
 
         attachTabLayout();
+    }
+
+    private void attachToolbar() {
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     private void attachTabLayout() {
---
+++ app/src/main/java/com/example/mynews/controllers/SearchActivity.java
@@ -0,0 +1,28 @@
+package com.example.mynews.controllers;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.mynews.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        attachToolbar();
+    }
+
+    private void attachToolbar() {
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
